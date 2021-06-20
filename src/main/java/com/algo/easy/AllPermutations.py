abd187e73fb102684b3be8847003c4a3e9baa830
sourabh raghav <sourabh.raghav@accenture.com>
2021-06-20 19:25:09 +0700

    Permutation & LIS

---
+++ src/main/java/com/algo/easy/AllPermutations.py
@@ -0,0 +1,7 @@
+def findByRec(start, end):
+    if len(end) == 1:
+        print(start + end)
+    for i in range(len(end)):
+        findByRec(start + end[i], end[0:i] + end[i + 1:])
+
+findByRec("", "ABCD")
---
+++ src/main/java/com/algo/medium/LongestIncrementingSubSequence.py
@@ -0,0 +1,69 @@
+def max(a, b):
+    if len(a) < len(b):
+        return b
+    else:
+        return a
+
+
+def printMat(mat):
+    print()
+    for i in mat:
+        for j in i:
+            print(j, end="\t")
+        print()
+
+
+def findByTabulation(input):
+    length = len(input)
+    T = [[] for i in input]
+    totalMax = 0
+    totalMaxList = []
+    T[0] = [input[0]]
+    for i in range(1, length):
+        max = 0
+        maxIndex = 0
+        for j in range(0, i + 1):
+            if input[i] >= input[j]:
+                if len(T[j]) > max:
+                    max = len(T[j])
+                    maxIndex = j
+
+        if max != 0:
+            T[i] = T[maxIndex] + [input[i]]
+        else:
+            T[i] = [input[i]]
+
+        if totalMax < len(T[i]):
+            totalMax = len(T[i])
+            totalMaxList = T[i]
+    return totalMaxList
+
+#brute force
+def findByRec(start, end, qb):
+    key = ":".join(str(x) for x in start + end)
+    if key in qb: return qb[key]
+    if len(start) == 0: return findByRec([end[0]], end[1:], qb)
+
+    if len(end) == 1:
+        qb[key] = start + [end[0]] if end[0] > start[len(start) - 1] else start
+        return qb[key]
+
+    else:
+        maxLen = findByRec(start, end[1:], qb)
+        if end[0] > start[len(start) - 1]:
+            qb[key] = max(maxLen, findByRec(start + [end[0]], end[1:], qb))
+            return qb[key]
+        else:
+            qb[key] = max(maxLen, findByRec([end[0]], end[1:], qb))
+            return qb[key]
+
+
+# input = [3, 4, -1, 0, 6, 2, 3, -2, -2, 4, 6]
+# input = [-1, 3, 4, 5, 2]
+# input = [2, 5, 1, 8, 3]
+# input = [3, 4, -1, 0, 6, 2, 3]
+# input = [3, 4, -1, 5, 8, 2, 3, 12, 7, 9, 10]
+input = [10, 22, 9, 33, 21, 50, 41, 60, 80, 3]
+
+print("Result : " + str(findByRec([], input, dict())))
+print("Result : " + str(findByTabulation(input)))
